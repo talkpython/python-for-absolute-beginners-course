d707c96b114f3a518a7b748a7429ced918e5f2cf
Michael Kennedy <mikeckennedy@gmail.com>
2020-01-11 15:43:45 -0800

    Getting input with a loop.

--- code/05-interactive-code/m-n-m/guessinggame.py
+++ code/05-interactive-code/m-n-m/guessinggame.py
@@ -4,4 +4,20 @@ print("------------------------------")
 print("     M&M guessing game!")
 print("------------------------------")
 
+print("Guess the number of M&Ms and you get lunch on the house!")
+print()
+
 mm_count = random.randint(1, 100)
+attempt_limit = 5
+attempts = 0
+
+while attempts < attempt_limit:
+    guess_text = input("How many M&Ms are in the jar? ")
+    guess = int(guess_text)
+    print(guess)
+
+    attempts += 1
+
+print("Bye")
+
+
