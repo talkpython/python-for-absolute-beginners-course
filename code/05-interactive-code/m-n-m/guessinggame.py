76a0703829a174c5602b68345e34cf1606923c33
Michael Kennedy <mikeckennedy@gmail.com>
2020-01-11 15:56:49 -0800

    Exit on win.

--- code/05-interactive-code/m-n-m/guessinggame.py
+++ code/05-interactive-code/m-n-m/guessinggame.py
@@ -7,23 +7,21 @@ print("------------------------------")
 print("Guess the number of M&Ms and you get lunch on the house!")
 print()
 
-mm_count = random.randint(1, 100)
+mm_count = 7  # random.randint(1, 100)
 attempt_limit = 5
 attempts = 0
 
 while attempts < attempt_limit:
     guess_text = input("How many M&Ms are in the jar? ")
     guess = int(guess_text)
+    attempts += 1
 
     if mm_count == guess:
         print(f"You got a free lunch! It was {guess}.")
+        break
     elif guess < mm_count:
         print("Sorry, that's too LOW!")
     else:
         print("That's too HIGH!")
 
-    attempts += 1
-
-print("Bye")
-
-
+print(f"Bye, you're done in {attempts}!")
