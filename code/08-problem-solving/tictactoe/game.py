a1fb700733844240854648ebbda58fc542171537
Michael Kennedy <mikeckennedy@gmail.com>
2020-01-30 11:28:20 -0800

    Tic tac toe, choose location done.

--- code/08-problem-solving/tictactoe/game.py
+++ code/08-problem-solving/tictactoe/game.py
@@ -2,9 +2,9 @@
 # √ create the board
 # √ choose an initial player
 # √ until someone wins, check for winner
-#    show the board
-#    choose location, mark it
-#    toggle active player
+# √   show the board
+# √   choose location, mark it
+#     toggle active player
 
 # game over, active player won!
 
@@ -21,12 +21,54 @@ def main():
 
     # CHOOSE INITIAL PLAYER
     active_player_index = 0
-    players = ["You", "Computer"]
+    players = ["Michael", "Computer"]
     symbols = ["X", "O"]
 
     # UNTIL SOMEONE WINS
     while not find_winner(board):
-        print("Play a round...")
+        # SHOW THE BOARD
+        player = players[active_player_index]
+        symbol = symbols[active_player_index]
+
+        announce_turn(player)
+        show_board(board)
+        if not choose_location(board, symbol):
+            print("That isn't an option, try again.")
+            continue
+
+
+def choose_location(board, symbol):
+    row = int(input("Choose which row: "))
+    column = int(input("Choose which column: "))
+
+    row -= 1
+    column -= 1
+    if row < 0 or row >= len(board):
+        return False
+    if column < 0 or column >= len(board[0]):
+        return False
+
+    cell = board[row][column]
+    if cell is not None:
+        return False
+
+    board[row][column] = symbol
+    return True
+
+
+def show_board(board):
+    for row in board:
+        print("| ", end='')
+        for cell in row:
+            symbol = cell if cell is not None else "_"
+            print(symbol, end=" | ")
+        print()
+
+
+def announce_turn(player):
+    print()
+    print(f"It's {player}'s turn. Here's the board:")
+    print()
 
 
 def find_winner(board):
