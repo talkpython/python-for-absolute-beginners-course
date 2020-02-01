8eecdae14900e38afb7854a6be695ddb31303ea9
Michael Kennedy <mikeckennedy@gmail.com>
2020-02-01 09:56:00 -0800

    Upadte the header title.

--- code/10-external-libraries/rocks-game/rpsgame.py
+++ code/10-external-libraries/rocks-game/rpsgame.py
@@ -23,7 +23,7 @@ def main():
 def show_header():
     print("---------------------------")
     print(" Rock Paper Scissors v2")
-    print(" Extensible Rules Edition")
+    print(" External Libraries Edition")
     print("---------------------------")
 
 
