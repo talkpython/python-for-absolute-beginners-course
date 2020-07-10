3fdffed5b4024df2f450f551a30ba02e46f28a0e
Michael Kennedy <mikeckennedy@gmail.com>
2020-07-10 16:27:53 -0700

    Solution for chapter 7 data structure.

---
+++ solutions/ch-07-data-structures/dictionary_program.py
@@ -0,0 +1,22 @@
+#####################################################################
+#
+# The essence of this practice is to create this data structure here:
+#
+d = {
+    'Sam': 7,
+    'rolls': ['rock', 'paper', 'scissors'],
+    'done': True
+}
+
+#####################################################################
+#
+# This is unchanged from the instructions,
+# sans formatting and display of output of expected.
+# \t means tab in Python strings.
+#
+
+print(d["Sam"], "\t\t\t\t\t\t\t\t# <- outputs 7?")
+print(d['rolls'], "\t# <- outputs ['rock', 'paper', 'scissors']?")
+print(d.get('Sarah'), "\t\t\t\t\t\t\t# <- outputs None?")
+print(d.get('Jeff', -1), "\t\t\t\t\t\t\t\t# <- outputs -1?")
+print(d['done'], "\t\t\t\t\t\t\t# <- outputs True?")
