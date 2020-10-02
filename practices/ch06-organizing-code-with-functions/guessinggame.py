18dc6ae0966416b0eddb2472a467718f1e3a0412
PP6975H <52664716+PP6975H@users.noreply.github.com>
2020-10-02 08:33:32 +0530

    Update guessinggame.py

--- practices/ch06-organizing-code-with-functions/guessinggame.py
+++ practices/ch06-organizing-code-with-functions/guessinggame.py
@@ -12,16 +12,19 @@ attempt_limit = 5
 attempts = 0
 
 while attempts < attempt_limit:
-    guess_text = input("How many M&Ms are in the jar? ")
-    guess = int(guess_text)
-    attempts += 1
+    try:
+        guess_text = input("How many M&Ms are in the jar? ")
+        guess = int(guess_text)
+        attempts += 1
 
-    if mm_count == guess:
-        print(f"You got a free lunch! It was {guess}.")
-        break
-    elif guess < mm_count:
-        print("Sorry, that's too LOW!")
-    else:
-        print("That's too HIGH!")
+        if mm_count == guess:
+            print(f"You got a free lunch! It was {guess}.")
+            break
+        elif guess < mm_count:
+            print("Sorry, that's too LOW!")
+        else:
+            print("That's too HIGH!")
 
-print(f"Bye, you're done in {attempts}!")
+    print(f"Bye, you're done in {attempts}!")
+    except:
+        print("Enter an integer Value !")
